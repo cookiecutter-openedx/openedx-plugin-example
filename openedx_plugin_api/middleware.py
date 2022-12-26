5996f9cef9febfbe74fe282c0095ea7dd654b15c
lpm0073 <lpm0073@gmail.com>
2022-12-26 12:39:15 -0600

    code APIRedirectMiddleware

--- openedx_plugin_api/middleware.py
+++ openedx_plugin_api/middleware.py
@@ -2,46 +2,31 @@ import logging
 
 from django.shortcuts import redirect
 from django.conf import settings
+from django.utils.deprecation import MiddlewareMixin
 
-import urllib.parse
+from edx_toggles.toggles import WaffleSwitch
 
 log = logging.getLogger(__name__)
 
+# define a Waffle flag for toggling whether to override the stock openedx api functionality
+OVERRIDE_OPENEDX_API_SWITCH = WaffleSwitch('openedx_plugin_example.override_openedx_api', "openedx_plugin_api")
 
-class LoginRedirectMiddleware:
+
+class APIRedirectMiddleware(MiddlewareMixin):
     def __init__(self, get_response):
         self.get_response = get_response
+        super().__init__()
 
     def __call__(self, request):
-        response = self.get_response(request)
-        if request.user.is_authenticated:
-            return response
+        # require a Waffle flag to enable overrides of the stock openedx api functionality
+        if not OVERRIDE_OPENEDX_API_SWITCH.is_enabled:
+            return self.get_response(request)
 
-        # mcdaniel aug-2022:
-        #   we should only redirect lms logins.
-        #   requests from other applications like say, studio or mfe authenticate
-        #   via internal oauth to the lms.
-        if request.META["HTTP_HOST"] not in [settings.OPENEDX_COMPLETE_DOMAIN_NAME]:
-            return response
-
-        if request.path.startswith("/login"):
+        if request.path.startswith("/path/to/api/endpoint-to-override"):
             log.info(
-                "plugin_api.middleware.LoginRedirectMiddleware.__call__() evaluating host: {host} path: {path} next: {next}".format(
-                    host=request.META["HTTP_HOST"], path=request.path, next=request.GET.get("next", "")
+                "openedx_plugin_api.middleware.APIRedirectMiddleware.__call__() redirecting host: {host} path: {path}".format(
+                    host=request.META["HTTP_HOST"], path=request.path
                 )
             )
-
-            if "next" in request.GET:
-                # mcdaniel aug-2022:
-                # we should avoid logins to the Django admin page. these cause
-                # "too many redirects" errors.
-                # https://staging.global-communications-academy.com/login?next=/admin
-                if request.GET.get("next", "") == "/admin":
-                    return response
-
-                return redirect(
-                    "/auth/login/custom-oauth/?auth_entry=login&next=" + urllib.parse.quote(request.GET["next"])
-                )
-            return redirect("/auth/login/custom-oauth/?auth_entry=login&next=/")
-
-        return response
+            return redirect("/auth/login/custom-oauth/")
+        return self.get_response(request)
