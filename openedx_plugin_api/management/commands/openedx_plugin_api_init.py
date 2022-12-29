fc91ec73d50d6271022e7b8d5ec559b7e6931978
lpm0073 <lpm0073@gmail.com>
2022-12-29 16:07:12 -0600

    add manage.py commands

---
+++ openedx_plugin_api/management/commands/openedx_plugin_api_init.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+
+from ...waffle import waffle_init
+
+
+class Command(BaseCommand):
+    help = "Verifies initialization records for all Django models in this plugin"
+
+    def handle(self, *args, **options):
+        waffle_init()
